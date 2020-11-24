26676e1f4a06512c60beb04c0dc48a484725c289
Thoompje123 <thvrijn2002@gmail.com>
2020-11-24 13:37:07 +0100

    Added navigation

--- App/App.js
+++ App/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { Icon } from 'react-native-elements'
 
 
-import LoginScreen from './src/screens/LoginScreen';
-import RegisterScreen from './src/screens/RegisterScreen';
+// Screens
+import RegisterScreen from './src/screens/RegisterScreen'
+import LoginScreen from './src/screens/LoginScreen'
 
-export default function App() {
-	return (
-		<View style={styles.container}>
-			<RegisterScreen />
-			{/* <LoginScreen /> */}
-		</View>
-	);
+export const App = () => {
+	const Stack = createStackNavigator()
+
+	return <View style={{ flex: 1 }}>
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName={'register'} screenOptions={{ headerShown: false }} >
+				<Stack.Screen name={'register'} component={RegisterScreen} />
+				<Stack.Screen name={'login'} component={LoginScreen}  />
+			</Stack.Navigator>
+		</NavigationContainer>
+	</View>
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1
-	},
-});
+export default App
--- App/src/screens/LoginScreen.js
+++ App/src/screens/LoginScreen.js
@@ -1,45 +1,49 @@
 import React from 'react'
 import { SafeAreaView, Text, View, StyleSheet, TextInput } from 'react-native'
 import { Icon } from 'react-native-elements'
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
 
 // Components
 import * as Forms from '../components/Forms'
 import Colors from '../components/Colors'
-import RegisterScreen from './RegisterScreen'
+import { LinearGradient } from 'expo-linear-gradient'
 
-export const LoginScreen = () => {
-    return <SafeAreaView style={styles.container}>
-        <View style={styles.body}>
-            <View style={{ flex: 0.2, justifyContent: 'center' }}>
-                <Icon name={'keyboard-backspace'} style={{ alignSelf: 'flex-start' }} size={32} />
+export const LoginScreen = ({ navigation }) => {
+    return <LinearGradient style={{ flex: 1 }} colors={[Colors.grey[50], Colors.grey[300]]} end={[1, 0]} start={[0, 1]}>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.body}>
+                <View style={{ flex: 0.2, justifyContent: 'center' }}>
+                    <TouchableWithoutFeedback onPress={() => navigation.goBack()}>
+                        <Icon name={'keyboard-backspace'} style={{ alignSelf: 'flex-start' }} size={32} />
+                    </TouchableWithoutFeedback>
+                </View>
+                <View style={{ flex: 1, justifyContent: 'center' }}>
+                    <Text style={styles.title}>Let's sign you in.</Text>
+
+                    <Text style={styles.desciption}>Welcome back. {`\n`}You've been missed!</Text>
+                </View>
+
+                <View style={{ flex: 1, justifyContent: 'center' }}>
+                    <Forms.InputText label={'Email'} style={{ marginBottom: 16 }} placeholder={'Enter your email'} keyboardType={'email-address'} />
+
+                    <Forms.InputText label={'Password'} placeholder={'enter your password'} secureTextEntry={true} />
+                </View>
+
+                <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+                    <Text style={{ fontSize: 18, color: Colors.grey[500], marginBottom: 32, textAlign: 'center' }}>
+                        Dont't have an account? <Text style={{ color: Colors.grey[900] }} onPress={() => navigation.goBack()}>Register</Text>
+                    </Text>
+
+                    <Forms.SubmitButton title={'Sign in'} color={Colors.grey[900]} textColor={'white'} />
+                </View>
             </View>
-            <View style={{ flex: 1, justifyContent: 'center' }}>
-                <Text style={styles.title}>Let's sign you in.</Text>
-
-                <Text style={styles.desciption}>Welcome back. {`\n`}You've been missed!</Text>
-            </View>
-
-            <View style={{ flex: 1, justifyContent: 'center' }}>
-                <Forms.InputText label={'Email'} style={{ marginBottom: 16 }} placeholder={'Enter your email'} keyboardType={'email-address'} />
-
-                <Forms.InputText label={'Password'} placeholder={'enter your password'} secureTextEntry={true} />
-            </View>
-
-            <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-                <Text style={{ fontSize: 18, color: Colors.grey[500], marginBottom: 32, textAlign: 'center' }}>
-                    Dont't have an account? <Text style={{ color: Colors.grey[900] }}>Register</Text>
-                </Text>
-
-                <Forms.SubmitButton title={'Sign in'} color={Colors.grey[900]} textColor={'white'} />
-            </View>
-        </View>
-    </SafeAreaView>
+        </SafeAreaView>
+    </LinearGradient>
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: Colors.grey[100],
     },
     body: {
         flex: 1,
